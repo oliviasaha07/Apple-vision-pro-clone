dc017cbcdae190f57860d86cc2c4bdf279c2e64b
Olivia Saha <107919663+oliviasaha07@users.noreply.github.com>
2023-09-15 22:01:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 function loco(){
     gsap.registerPlugin(ScrollTrigger);
 
-
 const locoScroll = new LocomotiveScroll({
   el: document.querySelector("#main"),
   smooth: true
 });
 
-// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
 ScrollTrigger.scrollerProxy("#main", {
   scrollTop(value) {
     return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
